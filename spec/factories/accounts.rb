46a5748097576134e32b7920fbab3dc0083f5fe1
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-06-30 23:32:26 +0200

    Add password validator.

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :account do
     login { "test_login" }
-    password { "random_password" }
+    password { Faker::Internet.password(min_length: 8, max_length: 20) }
     master_server { nil }
     premium_points { 1 }
     email { Faker::Internet.email }
---
+++ spec/validators/tribalwars_password_validator_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe TribalwarsPasswordValidator do
+  before do
+    stub_const("Validatable", Class.new).class_eval do
+      include ActiveModel::Validations
+      attr_accessor :password
+
+      validates :password, tribalwars_password: true
+    end
+  end
+
+  let(:subject) { TribalwarsPasswordValidator.new(attributes: :password) }
+  let(:mock) { Validatable.new }
+
+  before(:each) { subject.validate_each(mock, 'password', password) }
+
+  context 'when password is not valid' do
+    context 'password too short' do
+      let(:password) { 'qwert1' }
+
+      it { expect(mock.errors[:password]).to eq(['Password must be at least 8 characters long']) }
+    end
+
+    context 'password does not contain number' do
+      let(:password) { Faker::Alphanumeric.alpha(number: 8) }
+
+      it { expect(mock.errors[:password]).to eq(['Password must contain at least 1 digit']) }
+    end
+  end
+
+  context 'when password is valid' do
+    let(:password) { Faker::Internet.password(min_length: 8, max_length: 20) }
+    it 'does not have errors' do
+      expect(mock.errors.count).to eq(0)
+    end
+  end
+end
