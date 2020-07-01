c098ea01af5c9bf23cee49e2d51dd35c9e051fd0
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-07-01 12:11:18 +0200

    Add password and login validation spec.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Account, type: :model do
   it { is_expected.not_to allow_value('garbage').for(:premium_points) }
   it { is_expected.not_to allow_value(-100).for(:premium_points) }
   it { is_expected.not_to allow_value(10.7).for(:premium_points) }
+
+  it { is_expected.to allow_value('qwertyu1').for(:password) }
+  it { is_expected.to allow_value('12345678').for(:password) }
+  it { is_expected.not_to allow_value('').for(:password) }
+  it { is_expected.not_to allow_value('short1').for(:password) }
+  it { is_expected.not_to allow_value('no_digits').for(:password) }
+
+  it { is_expected.to allow_value('user').for(:login) }
+  it { is_expected.to allow_value('1234').for(:login) }
+  it { is_expected.not_to allow_value('usr').for(:login) }
 end
