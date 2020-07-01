66b19d6a12e1761d366f5b466fc9eec7ce333251
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-07-01 22:45:37 +0200

    Add MasterServer#download_world_list.

--- app/models/master_server.rb
+++ app/models/master_server.rb
@@ -3,4 +3,15 @@ class MasterServer < ApplicationRecord
   has_many :worlds
 
   validates :link, presence: true
+
+  def download_world_list
+    client = Tribes::Client.new(master_server: link)
+    action = GetWorldListGlobal.new(client)
+    new_worlds = action.execute
+    new_worlds.each do |key, value|
+      next if worlds.find_by(name: key)
+
+      worlds.create!(name: key, link: value)
+    end
+  end
 end
--- spec/models/master_server_spec.rb
+++ spec/models/master_server_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe MasterServer, type: :model do
   it { is_expected.to have_many(:worlds) }
 
   it { is_expected.to validate_presence_of(:link) }
+
+  describe '#download_world_list' do
+    let(:client) { double }
+    let(:worlds) do
+      { "en107"=>"https://en107.tribalwars.net",
+        "en110"=>"https://en110.tribalwars.net",
+        "en111"=>"https://en111.tribalwars.net",
+        "en112"=>"https://en112.tribalwars.net",
+        "en113"=>"https://en113.tribalwars.net" }
+    end
+
+    before do
+      allow(Tribes::Client).to receive(:new).and_return(client)
+      allow(client).to receive(:world_list_global).and_return(worlds)
+    end
+
+    context 'master server exists' do
+      let(:subject) { create(:master_server) }
+
+      it { expect{subject.download_world_list}.to change{subject.worlds.count}.from(0).to(worlds.count) }
+    end
+  end
 end
