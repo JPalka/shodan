82e177709decce8a8b6b0ed9a0b3aec7e06e1067
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-07-14 22:48:04 +0200

    Add service object for listing workers.

--- app/scripts/test_worker_listing.rb
+++ app/scripts/test_worker_listing.rb
@@ -1,70 +1,67 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'bunny'
-class TestWorkerClient
-  attr_accessor :call_id, :response, :lock, :condition, :connection,
-                :channel, :server_queue_name, :reply_queue, :exchange
+# require 'bunny'
+# class TestWorkerClient
+#   attr_accessor :call_id, :response, :lock, :condition, :connection,
+#                 :channel, :server_queue_name, :reply_queue, :exchange
 
-  def initialize(server_queue_name)
-    @connection = Bunny.new(automatically_recover: false)
-    @connection.start
+#   def initialize(server_queue_name)
+#     @connection = Bunny.new(automatically_recover: false)
+#     @connection.start
 
-    @channel = connection.create_channel
-    @exchange = channel.default_exchange
-    @server_queue_name = server_queue_name
+#     @channel = connection.create_channel
+#     @exchange = channel.default_exchange
+#     @server_queue_name = server_queue_name
 
-    setup_reply_queue
-  end
+#     setup_reply_queue
+#   end
 
-  def call(action)
-    @call_id = generate_uuid
-    body = { 'action' => action }
-    exchange.publish(JSON.generate(body),
-                     routing_key: server_queue_name,
-                     correlation_id: call_id,
-                     reply_to: reply_queue.name)
+#   def call(action)
+#     @call_id = generate_uuid
+#     body = { 'action' => action }
+#     exchange.publish(JSON.generate(body),
+#                      routing_key: server_queue_name,
+#                      correlation_id: call_id,
+#                      reply_to: reply_queue.name)
 
-    # wait for the signal to continue the execution
-    lock.synchronize { condition.wait(lock) }
+#     # wait for the signal to continue the execution
+#     lock.synchronize { condition.wait(lock) }
 
-    response
-  end
+#     response
+#   end
 
-  def stop
-    channel.close
-    connection.close
-  end
+#   def stop
+#     channel.close
+#     connection.close
+#   end
 
-  private
+#   private
 
-  def setup_reply_queue
-    @lock = Mutex.new
-    @condition = ConditionVariable.new
-    that = self
-    @reply_queue = channel.queue('', exclusive: true)
+#   def setup_reply_queue
+#     @lock = Mutex.new
+#     @condition = ConditionVariable.new
+#     that = self
+#     @reply_queue = channel.queue('', exclusive: true)
 
-    reply_queue.subscribe do |_delivery_info, properties, payload|
-      if properties[:correlation_id] == that.call_id
-        that.response = payload
+#     reply_queue.subscribe do |_delivery_info, properties, payload|
+#       if properties[:correlation_id] == that.call_id
+#         that.response = payload
 
-        # sends the signal to continue the execution of #call
-        that.lock.synchronize { that.condition.signal }
-      end
-    end
-  end
+#         # sends the signal to continue the execution of #call
+#         that.lock.synchronize { that.condition.signal }
+#       end
+#     end
+#   end
 
-  def generate_uuid
-    # very naive but good enough for code examples
-    SecureRandom.uuid
-  end
-end
+#   def generate_uuid
+#     # very naive but good enough for code examples
+#     SecureRandom.uuid
+#   end
+# end
 
-client = TestWorkerClient.new('worker_manager')
+client = ListWorkers.new('worker_manager')
 
 puts ' [x] Requesting worker list'
-response = client.call('list_workers')
+response = client.call
 
 puts " [.] Got #{response}"
-
-client.stop
