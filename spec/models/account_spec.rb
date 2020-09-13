f6824dddd58611d56a1468e3150623d5aec0a7f6
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-09-13 22:47:28 +0200

    Add missing account tests.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -26,4 +26,25 @@ RSpec.describe Account, type: :model do
   it { is_expected.to allow_value('user').for(:login) }
   it { is_expected.to allow_value('1234').for(:login) }
   it { is_expected.not_to allow_value('usr').for(:login) }
+
+  describe '#active_worlds_belong_to_same_server' do
+    it 'invalidates record when account and active_worlds not on same server' do
+      account = create(:account)
+      worlds = create_list(:world, 5)
+      account.active_worlds.push(worlds.first)
+
+      account.validate
+      expect(account.valid?).to be(false)
+      expect(account.errors[:active_worlds]).to eq(['World must belong to the same server as account'])
+    end
+
+    it 'is valid when active_worlds belong to same server' do
+      master_server = create(:master_server)
+      worlds = create(:world, master_server: master_server)
+      account = create(:account, master_server: master_server)
+
+      account.validate
+      expect(account.valid?).to be(true)
+    end
+  end
 end
