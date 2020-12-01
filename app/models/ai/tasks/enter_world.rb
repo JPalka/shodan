4371365e20ad6476c179cf42e62b61e44ece84b3
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-12-01 21:48:34 +0100

    Fix adding main player to world if it did not exist.

--- app/models/ai/tasks/enter_world.rb
+++ app/models/ai/tasks/enter_world.rb
@@ -3,7 +3,6 @@
 module AI
   module Tasks
     class EnterWorld < TaskBase
-      # login does not require args
       def check_args
         raise ArgumentError, 'Argument "world_name" missing' unless @args[:world_name]
 
@@ -18,7 +17,7 @@ module AI
         game_service = GameActionsService.new(client)
         world = World.find_by(name: @args[:world_name])
         game_service.enter_world(world.name)
-        world.add_player(**game_service.player_info, account_id: @args[:account_id])
+        world.add_player(**game_service.player_info, 'account_id' => @args[:account_id])
         true
       end
     end
